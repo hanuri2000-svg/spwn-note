fe39885e9eac6c92490288d4e954db719bb1d3f2
hanuri2000-svg <hanuri2000@naver.com>
2026-09-12 15:06:50 +0900

    Restore original layout with accessible feminine palette

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "spawn-note-v1.1.0";
+const CACHE_NAME = "spawn-note-v1.1.1";
 const APP_FILES = ["./", "./index.html", "./ui.js", "./config.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
